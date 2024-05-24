ae9b1f72ef16a221e8e5c322aee758836a7517a1
egmtncakgn <89608701+egmtncakgn@users.noreply.github.com>
2024-05-24 22:50:45 +0300

    Proje dosyası ekle.

---
+++ Root.cs
@@ -0,0 +1,27 @@
+﻿using OpenCvSharp;
+
+namespace OpenCvC_deneme
+{
+    class Root
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Görüntü İşleme Deneme Uygulaması");
+            Console.WriteLine($"OpenCV Versiyonu: {Cv2.GetVersionString()}");
+
+            // Görüntüyü Yükle 
+            Mat resim = Cv2.ImRead("DosyaYolu");
+
+            // Görüntü yoksa mesaj ver
+            if (resim.Empty())
+            {
+                Console.WriteLine("Görüntü Yüklenemedi !");
+                return;
+            }
+
+            // Görüntüyü göster
+            Cv2.ImShow("Görüntü Ekranı", resim);
+            Cv2.WaitKey(0);
+        }
+    }
+}
